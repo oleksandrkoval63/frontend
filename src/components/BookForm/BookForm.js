4d0997386c17e76e545ff3c3631614cd5a7e8400
Dean Metwise <deanmetwise@gmail.com>
2024-03-18 14:01:58 +0200

    modified:   src/components/BookForm/BookForm.js 	modified:   src/components/BookList/BookList.js

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import {v4 as uuidv4} from 'uuid'
 import { addBook } from '../../redux/books/actionCreators'
 import "./BookForm.css";
 
@@ -14,7 +15,8 @@ const BookForm = () => {
       if (title && author) {
          const book = {
             title: title,
-            author: author
+            author: author,
+            id: uuidv4()
          }
          dispatch(addBook(book))
 
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -9,12 +9,10 @@ const BookList = () => {
    return (
       <div className="app-block book-list">
          <h2>Book List</h2>
-         {books.lengh === 0 ? (
-            <p>No books available</p>
-         ) : (
+         {books.length === 0 ? (<p>No books available</p>) : (
             <ul>
                {books.map((book, i)=>(
-                  <li key={i}>
+                  <li key={book.id}>
                      <div className="book-info">{++i}. {book.title} by <strong>{book.author}</strong></div>
                   </li>
                ))}
